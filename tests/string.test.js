d9946d8729e8785215e74837751f1c58d945c9ee
Samuele <samueleferrari12@gmail.com>
2025-05-19 15:58:15 +0200

    added 3 test case for function lowercase

--- tests/string.test.js
+++ tests/string.test.js
@@ -1,4 +1,4 @@
-const {concat, rmspace, capitols} = require('../src/string');
+const {concat, rmspace, capitols, lowercase} = require('../src/string');
 
 
 test('concat "ciao" + "ciao" to equal "ciaociao"', ()=>{
@@ -27,4 +27,16 @@ test('Upper case of "cane" to equal "CANE"', ()=>{
 
 test('Upper case of "CANE" to equal "CANE"', ()=>{
     expect(capitols("CANE")).toBe("CANE");
+});
+
+test('Upper case of "CANE" to equal "cane"', ()=>{
+    expect(lowercase("CANE")).toBe("cane");
+});
+
+test('Upper case of "cane" to equal "cane"', ()=>{
+    expect(lowercase("cane")).toBe("cane");
+});
+
+test('Upper case of "CIAO come stai?" to equal "ciao come stai?"', ()=>{
+    expect(lowercase("CIAO come stai?")).toBe("ciao come stai?");
 });
